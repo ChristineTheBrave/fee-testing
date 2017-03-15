651dbd882aa8cc4d96777e4ae0b8bf8dde7b0777
Christine Ash <ChristineTheBrave@Christines-MacBook-Pro.local>
2017-03-15 14:48:24 -0400

    Created initial test for calc.js and calc.spec.js files

--- test/specs/calc.spec.js
+++ test/specs/calc.spec.js
@@ -2,9 +2,17 @@
     'use strict';
 
 
-    // TODO: Your tests go here!
+    describe('math modlue', function() {
+      describe('um function', function() {
+        it('should give the sum when provided two numbers', function() {
+          let result = window.calc.sum([1,2]);
+          expect( result ).to.be.a('number');
+          expect( result ).to.equal(3);
+        });
+      });
+    });
 
-    
+
 
 
 })();
